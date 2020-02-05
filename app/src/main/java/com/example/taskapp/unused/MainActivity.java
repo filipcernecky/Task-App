c9fa17fe9ec655a6cc82e89ca159fd3591c4797f
cerneckyf <filip.cernecky@akademiasovy.sk>
2020-02-05 22:31:04 +0100

    final

---
+++ app/src/main/java/com/example/taskapp/unused/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.taskapp.unused;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.taskapp.R;
+
+/*
+App icon created by Pixel Perfect
+Function concept created by Ferdousur Rahman Sarker (androstock)
+Design concept by Angga Risky
+Final App created by Filip Černecký (Viper) and Štefan Rothmayer (Stephaaan)
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
